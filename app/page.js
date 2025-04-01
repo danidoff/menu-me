7522b678bf3781b4269d4505095c978cf5519e09
Daniel Chicea <danielc@directferries.com>
2025-04-01 16:29:17 +0300

    [chaos] Changes to pdf

--- app/page.js
+++ app/page.js
@@ -6,11 +6,11 @@ import "../styles/home.css";
 const PDFViewer = dynamic(() => import("./components/PDFViewer"), { ssr: false });
 
 export default function Home() {
-  const [selectedPDF, setSelectedPDF] = useState("/pdfs/summer-breeze.pdf");
+  const [selectedPDF, setSelectedPDF] = useState("/pdfs/summer_breeze.pdf");
 
   return (
       <div className="home-container">
-          <PDFViewer file={"/pdfs/summer-breeze.pdf"} />
+          <PDFViewer file={"/pdfs/summer_breeze.pdf"} />
       </div>
   );
 }
