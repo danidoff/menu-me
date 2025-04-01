0eff328413fd55b090cc1ea4e1274b6d68772c42
Daniel Chicea <danielc@directferries.com>
2025-04-01 17:44:53 +0300

    [chaos] set supense 2

---
+++ app/components/PDFPage.js
@@ -0,0 +1,24 @@
+"use client";
+import { useSearchParams } from "next/navigation";
+import dynamic from "next/dynamic";
+import "../../styles/home.css";
+
+const PDFViewer = dynamic(() => import("./PDFViewer"), { ssr: false });
+
+export default function PDFPage() {
+    const searchParams = useSearchParams();
+    const pdfId = searchParams.get("id");
+
+    const pdfMapping = {
+        "summerbreeze": "/pdfs/summer-breeze.pdf",
+        "laturn": "/pdfs/la-turn.pdf",
+    };
+
+    const fileUrl = pdfId && pdfMapping[pdfId] ? pdfMapping[pdfId] : null;
+
+    return (
+        <div className="home-container ">
+            <PDFViewer file={fileUrl} />
+        </div>
+    );
+}
--- app/page.js
+++ app/page.js
@@ -1,29 +1,13 @@
 "use client";
-import {useSearchParams} from "next/navigation";
-import dynamic from "next/dynamic";
 import {Suspense} from "react";
 import "../styles/home.css";
+import PDFPage from "@/app/components/PDFPage";
 
-const PDFViewer = dynamic(() => import("./components/PDFViewer"), {ssr: false});
 
 export default function Home() {
-    const searchParams = useSearchParams();
-    const pdfId = searchParams.get("id");
-
-    const pdfMapping = {
-        "summerbreeze": "/pdfs/summer-breeze.pdf",
-        "laturn": "/pdfs/la-turn.pdf",
-    };
-
-    const fileUrl = pdfId && pdfMapping[pdfId] ? pdfMapping[pdfId] : null;
-
     return (
         <Suspense fallback={<div>Loading...</div>}>
-            <div className="home-container ">
-
-                <PDFViewer file={fileUrl}/>
-
-            </div>
+            <PDFPage />
         </Suspense>
     );
 }
